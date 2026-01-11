5b7e241ee3ed3b17489e666023d179e84524a9f1
KaylenTon <kaylent2k@gmail.com>
2026-01-11 10:59:48 -0500

    Changed from PSPAP call datetime to unit_notified_by_dispatch_datetime

---
+++ CALLS PER INTERVAL.R
@@ -0,0 +1,69 @@
+library(tidyverse)
+library(ggplot2)
+library(hms)
+
+# unique minute
+calls_per_interval_unique_minute <- time_df %>% 
+  select(PcrKey, unit_notified_by_dispatch_datetime) %>% 
+  mutate(PSAP_time = as_hms(unit_notified_by_dispatch_datetime)) %>% 
+  group_by(PSAP_time) %>% 
+  count() %>% 
+  arrange(desc(n)) %>% 
+  mutate(
+    hour = hour(PSAP_time),
+    interval_per_8 = hour %/% 8,
+    interval_per_8 = case_when(
+      interval_per_8 == 0 ~ "A", #00:00:00 - 07:59:00
+      interval_per_8 == 1 ~ "B", #08:00:00 - 15:59:00
+      interval_per_8 == 2 ~ "C", #16:00:00 - 23:59:00
+    )
+  )
+
+calls_per_interval_unique_minute %>% 
+  group_by(interval_per_8) %>% 
+  count()
+
+ggplot(calls_per_interval_unique_minute, aes(x = PSAP_time, y = n, color = interval_per_8)) + geom_line() + scale_y_continuous(limits = c(0, 5))
+
+
+# unique hour
+calls_per_interval_unique_hour <- time_df %>% 
+  select(PcrKey, unit_notified_by_dispatch_datetime) %>% 
+  mutate(unit_notified_hour = round_date(unit_notified_by_dispatch_datetime, "hour")) %>% 
+  mutate(unit_notified_time = as_hms(unit_notified_hour)) %>% 
+  group_by(unit_notified_time) %>% 
+  count() %>% 
+  arrange(desc(n)) %>% 
+  mutate(
+    hour = hour(unit_notified_time),
+    interval_per_8 = hour %/% 8,
+    interval_per_8 = case_when(
+      interval_per_8 == 0 ~ "00:00:00 - 07:59:00", #00:00:00 - 07:59:00
+      interval_per_8 == 1 ~ "08:00:00 - 15:59:00", #08:00:00 - 15:59:00
+      interval_per_8 == 2 ~ "16:00:00 - 23:59:00", #16:00:00 - 23:59:00
+    )
+  )
+
+ggplot(calls_per_interval_unique_hour, aes(x = unit_notified_time, y = n, color = interval_per_8)) + 
+  geom_line(size = .5, linetype = "dashed") + 
+  geom_point(size = 2) +
+  scale_y_continuous(limits = c(0,125)) +
+  labs(
+    title = "Calls per interval using:  unit_notified_by_dispatch_datetime",
+    subtitle = "Count Time Series",
+    x = "Time", 
+    y = "Call Count",
+    color = "Time interval"
+  ) +
+  theme_light() +
+  geom_text(
+    aes(label = n),
+    vjust = -1,
+    size = 4,
+    color = "black"
+  )
+
+calls_per_interval_unique_hour %>% 
+  group_by(interval_per_8) %>% 
+  count() %>% 
+  summarise(sum = sum(n)) # ERROR
