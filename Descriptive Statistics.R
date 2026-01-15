9d8c8f1e71485d2b260c41166c6a310558ee3748
KaylenTon <kaylent2k@gmail.com>
2026-01-14 22:20:19 -0500

    created csv for frequency tables of most event and patient variables

--- Descriptive Statistics.R
+++ Descriptive Statistics.R
@@ -3,160 +3,202 @@ library(ggplot2)
 
 # Event variables ----------------------------------------------------------------
 
-  # dispatch_reason
-  dispatch_reason_table <- event_df %>%
-    count(dispatch_reason) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
+a <- event_df %>%
+  count(dispatch_reason) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "dispatch_reason"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = dispatch_reason)
 
-  ggplot(dispatch_reason_table, aes(x = reorder(dispatch_reason, n), y = n)) + 
-    geom_col(show.legend = F) + 
-    coord_flip() +
-    labs(
-      title = "Dispatch Reason",
-      x = NULL,
-      y = "Count"
-    ) +
-    geom_text(
-      aes(label = n),
-      hjust = -.35,
-      size = 4,
-      color = "black"
-    )
-  
-  # EMD_performed
-  event_df %>%
-    count(EMD_performed) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # acuity_upon_EMS_release_of_patient
-  event_df %>%
-    count(acuity_upon_EMS_release_of_patient) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # EMS_transport_method
-  event_df %>%
-    count(EMS_transport_method) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # type_of_destination
-  event_df %>%
-    count(type_of_destination) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # hospital_in_patient_destination
-  event_df %>%
-    count(hospital_in_patient_destination) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # level_of_care_provided_per_protocol
-  event_df %>%
-    count(level_of_care_provided_per_protocol) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # unit_disposition
-  event_df %>%
-    count(unit_disposition) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # patient_evaluation_care
-  event_df %>%
-    count(patient_evaluation_care) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # crew_disposition
-  event_df %>%
-    count(crew_disposition) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # transport_disposition
-  event_df %>%
-    count(transport_disposition) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # transport_mode_from_scene
-  event_df %>%
-    count(transport_mode_from_scene) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # # datetime_of_destination_prearrival_alert_or_activation
-  # event_df %>%
-  #   count(datetime_of_destination_prearrival_alert_or_activation) %>%
-  #   mutate(proportion = n / sum(n)) %>% 
-  #   arrange(desc(proportion)) %>%
-  #   print(n = nrow(.))
-  
-  # # desination_team_prearrival_alert_or_activation
-  # event_df %>%
-  #   count(desination_team_prearrival_alert_or_activation) %>%
-  #   mutate(proportion = n / sum(n)) %>% 
-  #   arrange(desc(proportion)) %>%
-  #   print(n = nrow(.))
-  
-  # type_of_service_requested
-  event_df %>%
-    count(type_of_service_requested) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # unit_transport_and_equipment_capability
-  event_df %>%
-    count(unit_transport_and_equipment_capability) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # response_mode_to_scene
-  event_df %>%
-    count(response_mode_to_scene) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # type_of_turn_around_delay
-  event_df %>%
-    count(type_of_turn_around_delay) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # type_of_scene_delay
-  event_df %>%
-    count(type_of_scene_delay) %>%
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))
-  
-  # # type_of_response_delay
-  # event_df %>%
-  #   count(type_of_response_delay) %>%
-  #   mutate(proportion = n / sum(n)) %>%
-  #   arrange(desc(proportion)) %>%
-  #   print(n = nrow(.))
+b <- event_df %>%
+  count(EMD_performed) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "EMD_performed"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = EMD_performed)
+
+c <- event_df %>%
+  count(acuity_upon_EMS_release_of_patient) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "acuity_upon_EMS_release_of_patient"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = acuity_upon_EMS_release_of_patient)
+
+d <- event_df %>%
+  count(EMS_transport_method) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "EMS_transport_method"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = EMS_transport_method)
+
+e <- event_df %>%
+  count(type_of_destination) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "type_of_destination"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = type_of_destination)
+
+f <- event_df %>%
+  count(hospital_in_patient_destination) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "hospital_in_patient_destination"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = hospital_in_patient_destination)
+
+g <- event_df %>%
+  count(level_of_care_provided_per_protocol) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "level_of_care_provided_per_protocol"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = level_of_care_provided_per_protocol)
+
+h <- event_df %>%
+  count(unit_disposition) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "unit_disposition"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = unit_disposition)
+
+i <- event_df %>%
+  count(patient_evaluation_care) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "patient_evaluation_care"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = patient_evaluation_care)
+
+j <- event_df %>%
+  count(crew_disposition) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "crew_disposition"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = crew_disposition)
+
+k <- event_df %>%
+  count(transport_disposition) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "transport_disposition"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = transport_disposition)
+
+l <- event_df %>%
+  count(transport_mode_from_scene) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "transport_mode_from_scene"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = transport_mode_from_scene)
+
+n <- event_df %>%
+  count(desination_team_prearrival_alert_or_activation) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "desination_team_prearrival_alert_or_activation"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = desination_team_prearrival_alert_or_activation)
+
+o <- event_df %>%
+  count(type_of_service_requested) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "type_of_service_requested"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = type_of_service_requested)
+
+
+p <- event_df %>%
+  count(unit_transport_and_equipment_capability) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "unit_transport_and_equipment_capability"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = unit_transport_and_equipment_capability)
+
+q <- event_df %>%
+  count(response_mode_to_scene) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "response_mode_to_scene"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = response_mode_to_scene)
+
+r <- event_df %>%
+  count(type_of_turn_around_delay) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "type_of_turn_around_delay"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = type_of_turn_around_delay)
+
+s <- event_df %>%
+  count(type_of_scene_delay) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "type_of_scene_delay"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = type_of_scene_delay)
+
+t <- event_df %>%
+  count(type_of_response_delay) %>%
+  mutate(
+    proportion = n / sum(n),
+    table = "event_df",
+    variable = "type_of_response_delay"
+  ) %>%
+  arrange(desc(n)) %>% 
+  rename(value = type_of_response_delay)
+
+
+# Time variables ----------------------------------------------------------
+
+summary(time_df$time_resolve_issue)
+
 
 # Patient variables -------------------------------------------------------
   
@@ -174,22 +216,44 @@ library(ggplot2)
   summary(patient_df$patient_age_years)
   
   # patient_race
-  patient_df %>% 
-      count(patient_race) %>% 
-      mutate(proportion = n / sum(n)) %>% 
-      arrange(desc(proportion)) %>% 
-      print(n = nrow(.))
+  u <- patient_df %>%
+    count(patient_race) %>%
+    mutate(
+      proportion = n / sum(n),
+      table = "patient_df",
+      variable = "patient_race"
+    ) %>%
+    arrange(desc(n)) %>% 
+    rename(value = patient_race)
   
   # age_group
-  patient_df %>% 
-      count(age_group) %>% 
-      mutate(proportion = n / sum(n)) %>% 
-      arrange(desc(proportion)) %>% 
-      print(n = nrow(.))
+  v <- patient_df %>%
+    count(age_group) %>%
+    mutate(
+      proportion = n / sum(n),
+      table = "patient_df",
+      variable = "age_group"
+    ) %>%
+    arrange(desc(n)) %>% 
+    rename(value = age_group)
   
   # age_decade_group
-  patient_df %>% 
-    count(age_decade_group) %>% 
-    mutate(proportion = n / sum(n)) %>% 
-    arrange(desc(proportion)) %>% 
-    print(n = nrow(.))  
+  w <- patient_df %>%
+    count(age_decade_group) %>%
+    mutate(
+      proportion = n / sum(n),
+      table = "patient_df",
+      variable = "age_decade_group"
+    ) %>%
+    arrange(desc(n)) %>% 
+    rename(value = age_decade_group)
+
+
+# combine tables ----------------------------------------------------------
+
+frequency_tables <- bind_rows(
+  a, b, c, d, e, f, g, h, i, j, k, l, n, o, p, q, r, s, t, u, v, w
+) %>% 
+  select(table, variable, value, n, proportion)
+
+write.csv(frequency_tables, "frequency_tables.csv")
