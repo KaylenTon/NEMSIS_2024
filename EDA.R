d5ded259182726759e399a079302fd501db844c5
KaylenTon <kaylent2k@gmail.com>
2026-01-22 23:01:35 -0500

    improved stacked bar chart. faceted by age group

--- EDA.R
+++ EDA.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(ggplot2)
 library(hms)
+library(RColorBrewer)
+
+display.brewer.all()
 
 # RQ: Is there a significant difference between the younger and older age groups regarding:
 #       1. dispatch_reason
@@ -30,13 +33,24 @@ focus_data <- from_event %>%
 
 # Count time series + dispatch reason + age groups ------------------------
 
+top_10_dispatch_reasons <- c("Sick Person",
+                             "Transfer/Interfacility/Palliative Care",
+                             "Breathing Problem",
+                             "Falls",
+                             "No Other Appropriate Choice",
+                             "Chest Pain (Non-Traumatic)",
+                             "Traffic/Transportation Incident",
+                             "Unconscious/Fainting/Near-Fainting",
+                             "Unknown Problem/Person Down",
+                             "Psychiatric Problem/Abnormal Behavior/Suicide Attempt")
+
 test <- focus_data %>% 
-  select(PcrKey, dispatch_reason, unit_notified_by_dispatch_datetime) %>% 
+  select(PcrKey, dispatch_reason, unit_notified_by_dispatch_datetime, age_group) %>% 
+  filter(dispatch_reason %in% top_10_dispatch_reasons) %>% 
   mutate(unit_notified_time = as_hms(round_date(unit_notified_by_dispatch_datetime, "hour"))) %>% 
-  group_by(unit_notified_time) %>% 
-  mutate(
-    count = n()
-  )
+  group_by(unit_notified_time, dispatch_reason, age_group) %>% 
+  summarise(count = n(), .groups = "drop") %>% 
+  arrange(desc(count))
   #   %>%
   # mutate(
   #   hour = hour(unit_notified_time),
@@ -48,5 +62,22 @@ test <- focus_data %>%
   #   )
   # )
 
-ggplot(test, aes(x = unit_notified_time, y = count, .group = dispatch_reason, fill = dispatch_reason)) + geom_col(position = "stack")
-  
+ggplot(na.omit(test), aes(x = unit_notified_time, y = count, .group = dispatch_reason, fill = dispatch_reason)) + 
+  geom_vline(xintercept = c(28800, 28800 * 2), linetype = "dashed", color = "black", linewidth = 1) +
+  geom_col(position = "stack") +
+  scale_fill_brewer(palette = "Paired") +
+  scale_y_continuous(limits = c(0,15), n.breaks = 5) +
+  facet_wrap(~age_group) +
+  theme_light() +
+  labs(
+    title = "Top 10 Dispacth Reasons At Each Hour",
+    subtitle = "418 observations",
+    x = "Time",
+    y = "Count",
+    fill = "Dispacth Reason"
+  ) +
+  geom_text(
+    aes(label = count),
+    position = position_stack(vjust = .5),
+    color = "white"
+  )
