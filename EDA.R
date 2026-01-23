3581312012d1fc2ef6f498490d157071014538ad
KaylenTon <kaylent2k@gmail.com>
2026-01-23 09:18:19 -0500

    (commented out) code for visual summary validations

--- EDA.R
+++ EDA.R
@@ -50,7 +50,7 @@ reason_per_age_group_and_hour <- focus_data %>%
   filter(dispatch_reason %in% top_10_dispatch_reasons) %>% 
   mutate(unit_notified_time = as_hms(round_date(unit_notified_by_dispatch_datetime, "hour"))) %>% 
   group_by(unit_notified_time, dispatch_reason, age_group) %>% 
-  summarise(count = n(), .groups = "drop") %>% 
+  summarize(count = n(), .groups = "drop") %>% 
   arrange(desc(count))
   #   %>%
   # mutate(
@@ -90,23 +90,23 @@ ggplot(na.omit(reason_per_age_group_and_hour), aes(x = unit_notified_time, y = c
 
   # Top 10 dispatch reasons
 
-  time_per_age_group_and_reason <- focus_data %>% 
+  time_per_age_group_and_10_reasons <- focus_data %>% 
     select(PcrKey, dispatch_reason, age_group, time_resolve_issue) %>% 
     filter(dispatch_reason %in% top_10_dispatch_reasons) %>% 
     group_by(dispatch_reason, age_group) %>% 
-    summarise(avg_time = mean(time_resolve_issue), .groups = "drop") %>% 
+    summarize(avg_time = mean(time_resolve_issue), .groups = "drop") %>% 
     mutate(avg_time = round(avg_time)) %>% 
     arrange(desc(avg_time))
   
-  ggplot(na.omit(time_per_age_group_and_reason), aes(x = dispatch_reason, y = avg_time, .group = dispatch_reason, fill = dispatch_reason)) + 
-    geom_col(position = "dodge", show.legend = F) + 
+  ggplot(na.omit(time_per_age_group_and_10_reasons), aes(x = dispatch_reason, y = avg_time, .group = dispatch_reason, fill = dispatch_reason)) + 
+    geom_col(show.legend = F) + 
     scale_fill_brewer(palette = "Paired") +
     coord_flip() +
     facet_wrap(~age_group) +
     theme_light() +
     theme(legend.position = "bottom") +
     labs(
-      title = "Average Time Per Dispatch Reason",
+      title = "Average Time Per Per Top 10 Dispatch Reason",
       subtitle = "418 observations",
       x = NULL,
       y = "Minutes"
@@ -119,16 +119,16 @@ ggplot(na.omit(reason_per_age_group_and_hour), aes(x = unit_notified_time, y = c
 
   # ALL dispatch reasons
 
-  time_per_age_group_and_reason <- focus_data %>% 
+  time_per_age_group_and_all_reasons <- focus_data %>% 
     select(PcrKey, dispatch_reason, age_group, time_resolve_issue) %>% 
     #filter(dispatch_reason %in% top_10_dispatch_reasons) %>% 
     group_by(dispatch_reason, age_group) %>% 
-    summarise(avg_time = mean(time_resolve_issue), .groups = "drop") %>% 
+    summarize(avg_time = mean(time_resolve_issue), .groups = "drop") %>% 
     mutate(avg_time = round(avg_time)) %>% 
     arrange(desc(avg_time))
   
-  ggplot(na.omit(time_per_age_group_and_reason), aes(x = dispatch_reason, y = avg_time, .group = dispatch_reason, fill = dispatch_reason)) + 
-    geom_col(position = "dodge", show.legend = F) + 
+  ggplot(na.omit(time_per_age_group_and_all_reasons), aes(x = dispatch_reason, y = avg_time, .group = dispatch_reason, fill = dispatch_reason)) + 
+    geom_col(show.legend = F) + 
     #scale_fill_brewer(palette = "Paired") +
     coord_flip() +
     facet_wrap(~age_group) +
@@ -146,11 +146,18 @@ ggplot(na.omit(reason_per_age_group_and_hour), aes(x = unit_notified_time, y = c
       color = "white"
     )
 
-avg_time_by_reason_age <- focus_data %>%
-  filter(age_group %in% c("Senior", "Younger")) %>%
-  group_by(dispatch_reason, age_group) %>%
-  summarise(
-    avg_time_resolve = mean(time_resolve_issue, na.rm = TRUE),
-    n = sum(!is.na(time_resolve_issue)),
-    .groups = "drop"
-  )
+# avg_time_by_reason_age <- focus_data %>%
+#   filter(age_group %in% c("Senior", "Younger")) %>%
+#   group_by(dispatch_reason, age_group) %>%
+#   summarize(
+#     avg_time_resolve = mean(time_resolve_issue),
+#     .groups = "drop"
+#   )
+# 
+# focus_data %>%
+#   filter(
+#     age_group == "Younger",
+#     dispatch_reason == "Altered Mental Status"
+#   ) %>%
+#   summarize(avg_time_resolve = mean(time_resolve_issue, na.rm = TRUE)) %>%
+#   pull()
