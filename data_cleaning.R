465d651cf9ed9eae93d4b19d25070c31f4587554
KaylenTon <kaylent2k@gmail.com>
2026-01-24 14:21:51 -0500

    edited the cleaning process. DUPLICATES NEED TO REEVALUATED

--- data_cleaning.R
+++ data_cleaning.R
@@ -139,13 +139,25 @@ for (i in seq_along(select_paths)) {
 
 names(sas_data_list) <- file_path_sans_ext(basename(select_paths))
 
-use_data <- reduce(sas_data_list, left_join, by = "PcrKey") %>% 
-  distinct(PcrKey, .keep_all = TRUE)
+use_data <- reduce(sas_data_list, left_join, by = "PcrKey")
 
-# Renaming variables ------------------------------------------------------
+# Selecting/reordering variables ------------------------------------------
 
 select_data <- use_data %>% 
-  select(PcrKey, contains(c("Dispatch", "Disposition", "Response", "Times", "Patient_", "Crew"))) %>% 
+  select(PcrKey:EMSTotalCallTimeMin, contains(c("Dispatch", "Disposition", "Response", "Times", "Patient_", "Crew")))
+
+# Removing NAs ------------------------------------------------------------
+
+to_NA <- c("7701003", "7701001", "7701005", "Not Recorded", "Not Applicable")
+
+clean_NA <- select_data %>% 
+  mutate(across(everything(), ~ if_else(.x %in% to_NA, NA, .x)))
+
+
+# Event table -------------------------------------------------------------
+
+event_df <- clean_NA %>% 
+  select(PcrKey:Urbanicity, eDispatch_01:eResponse_09) %>% 
   rename(
     dispatch_reason = eDispatch_01,
     EMD_performed = eDispatch_02,
@@ -159,41 +171,15 @@ select_data <- use_data %>%
     crew_disposition = eDisposition_29,
     transport_disposition = eDisposition_30,
     transport_mode_from_scene = eDisposition_17,
-    datetime_of_destination_prearrival_alert_or_activation = eDisposition_25, # 100% NA
-    desination_team_prearrival_alert_or_activation = eDisposition_24, # 100% NA
+    datetime_of_destination_prearrival_alert_or_activation = eDisposition_25,
+    desination_team_prearrival_alert_or_activation = eDisposition_24,
     type_of_service_requested = eResponse_05,
-    unit_transport_and_equipment_capability = eResponse_07, # different type of ems vehicles may affect time to scene and time to destination
+    unit_transport_and_equipment_capability = eResponse_07,
     response_mode_to_scene = eResponse_23,
     type_of_turn_around_delay= eResponse_12,
     type_of_scene_delay = eResponse_10,
-    type_of_response_delay = eResponse_09, # 100% NA
-    PSAP_call_datetime = eTimes_01,
-    unit_notified_by_dispatch_datetime = eTimes_03,
-    unit_en_route_datetime = eTimes_05,
-    unit_arrived_on_scene_datetime = eTimes_06,
-    arrived_at_patient_datetime = eTimes_07,
-    unit_left_scene_datetime = eTimes_09,
-    patient_arrived_at_destination_datetime = eTimes_11,
-    destination_patient_transfer_of_care_datetime = eTimes_12,
-    unit_back_in_service_datetime = eTimes_13,
-    patient_age = ePatient_15,
-    patient_age_units = ePatient_16,
-    patient_race = ePatient_14
-  )
-
-
-# Removing NAs ------------------------------------------------------------
-
-to_NA <- c("7701003", "7701001", "7701005", "Not Recorded", "Not Applicable")
-
-clean_NA <- select_data %>% 
-  mutate(across(everything(), ~ if_else(.x %in% to_NA, NA, .x)))
-
-
-# Event table -------------------------------------------------------------
-
-event_df <- clean_NA %>% 
-  select(PcrKey, dispatch_reason:type_of_response_delay) %>% 
+    type_of_response_delay = eResponse_09,
+  ) %>% 
   mutate(
     dispatch_reason = recode(
       dispatch_reason,
@@ -507,7 +493,18 @@ event_df <- clean_NA %>%
 # Time table --------------------------------------------------------------
 
 time_df <- clean_NA %>% 
-  select(PcrKey, PSAP_call_datetime:unit_back_in_service_datetime) %>% 
+  select(PcrKey, EMSTransportTimeMin, EMSTotalCallTimeMin, eTimes_01:eTimes_13) %>% 
+  rename(
+    PSAP_call_datetime = eTimes_01,
+    unit_notified_by_dispatch_datetime = eTimes_03,
+    unit_en_route_datetime = eTimes_05,
+    unit_arrived_on_scene_datetime = eTimes_06,
+    arrived_at_patient_datetime = eTimes_07,
+    unit_left_scene_datetime = eTimes_09,
+    patient_arrived_at_destination_datetime = eTimes_11,
+    destination_patient_transfer_of_care_datetime = eTimes_12,
+    unit_back_in_service_datetime = eTimes_13
+  ) %>% 
   mutate(
     across(PSAP_call_datetime:unit_back_in_service_datetime, ~ as.POSIXct(fast_strptime(.x, format = "%d%b%Y:%H:%M:%S"))
   )) %>% 
@@ -528,7 +525,12 @@ time_df <- clean_NA %>%
 # Patient table -----------------------------------------------------------
 
 patient_df <- clean_NA %>% 
-  select(PcrKey, patient_age:patient_race) %>% 
+  select(PcrKey, ageinyear, ePatient_15:ePatient_14) %>% 
+  rename(
+    patient_age = ePatient_15,
+    patient_age_units = ePatient_16,
+    patient_race = ePatient_14
+  ) %>% 
   mutate(
     patient_age_units = recode(
       patient_age_units,
