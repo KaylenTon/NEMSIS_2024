29d33942930b8ed320e16bd7ea19369a5abde9ea
KaylenTon <kaylent2k@gmail.com>
2026-02-12 00:04:15 -0500

    releveling age_group via factor(x, levels = c()) instead of relevel()

--- data_cleaning.R
+++ data_cleaning.R
@@ -573,7 +573,7 @@ clean_NA <- clean_NA %>%
   # relevel factors
   mutate(
     age_group = relevel(age_group, "Younger", "Senior"),
-    age_interval_group = relevel(age_interval_group, "0-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80-84", "85+")
+    age_interval_group = factor(age_interval_group, levels = c("0-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80-84", "85+"))
   )
 
 #(needs review to simplify certain processes): 
