392b59b7540d349c45e41dc790a99797f943300e
Megan Xiao <xmegan10@gmail.com>
2026-03-15 13:22:52 -0400

    switch back to paste_by_underscore func

---
+++ megan_anova_alsbls.R
@@ -0,0 +1,8 @@
+library(ggplot)
+table(final_clean_NA$level_of_care_provided_per_protocol)
+
+
+#t-test for significant difference between level_of_care_provided using age as the continuous variable
+#type of service: eDisposition.32
+
+a_clean_NA <- subset()
