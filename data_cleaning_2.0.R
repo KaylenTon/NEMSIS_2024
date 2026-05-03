46694c5f06a65c49949f1d93ade1d8f458ae1a1c
KaylenTon <kaylent2k@gmail.com>
2026-05-03 16:52:41 -0400

    need to clean next

--- data_cleaning_2.0.R
+++ data_cleaning_2.0.R
@@ -29,11 +29,11 @@ select_paths <- c("C:/Users/Kaylen/OneDrive - University of South Florida/Docume
                   "C:/Users/Kaylen/OneDrive - University of South Florida/Documents/R PRACTICE/SAS2024CP25/factpcrscenedelay.sas7bdat",
                   "C:/Users/Kaylen/OneDrive - University of South Florida/Documents/R PRACTICE/SAS2024CP25/factpcrresponsedelay.sas7bdat",
                   "C:/Users/Kaylen/OneDrive - University of South Florida/Documents/R PRACTICE/SAS2024CP25/factpcrdestinationteam.sas7bdat",
-                  "C:/Users/Kaylen/OneDrive - University of South Florida/Documents/R PRACTICE/SAS2024CP25/esituation_11ref.sas7bdat",
-                  "C:/Users/Kaylen/OneDrive - University of South Florida/Documents/R PRACTICE/SAS2024CP25/factpcrprimaryimpression.sas7bdat")
+                  "C:/Users/Kaylen/OneDrive - University of South Florida/Documents/R PRACTICE/SAS2024CP25/factpcrprimaryimpression.sas7bdat",
+                  "C:/Users/Kaylen/OneDrive - University of South Florida/Documents/R PRACTICE/SAS2024CP25/esituation_11ref.sas7bdat")
 
 select_variables <- list(
-  # computedelements
+  # computedelements 1
   c("PcrKey",
     "USCensusRegion",
     "USCensusDivision",
@@ -42,7 +42,7 @@ select_variables <- list(
     "ageinyear",
     "EMSTransportTimeMin",
     "EMSTotalCallTimeMin"),
-  # pub_pcrevents
+  # pub_pcrevents 2
   c("PcrKey", 
     "eDispatch_01", 
     "eDispatch_02", 
@@ -63,13 +63,13 @@ select_variables <- list(
     "eDisposition_17",
     "eSituation_13",
     "eDisposition_19"),
-  # pcrpatientracegroup
+  # pcrpatientracegroup 3
   c("PcrKey", 
     "ePatient_14"),
-  # factpcrturnarounddelay
+  # factpcrturnarounddelay 4
   c("PcrKey", 
     "eResponse_12"),
-  # factpcrtime
+  # factpcrtime 5
   c("PcrKey", 
     "eTimes_01", 
     "eTimes_03", 
@@ -80,27 +80,27 @@ select_variables <- list(
     "eTimes_11", 
     "eTimes_12", 
     "eTimes_13"),
-  # factpcrscenedelay
+  # factpcrscenedelay 6
   c("PcrKey", 
     "eResponse_10"),
-  # factpcrresponsedelay
+  # factpcrresponsedelay 7
   c("PcrKey", 
     "eResponse_09"),
-  # factpcrdestinationteam
+  # factpcrdestinationteam 8
   c("PcrKey", 
     "eDisposition_25", 
     "eDisposition_24"),
-  # factpcrprimaryimpression
+  # factpcrprimaryimpression 9
   c("PcrKey",
     "eSituation_11"),
-  # esituation_11ref
+  # esituation_11ref 10
   c("eSituation_11",
     "DiagnosisCodeDescr")
 )
 
 sas_data_list <- list()
 
-for (i in seq_along(select_paths)) {
+for (i in seq_len(min(9, length(select_paths)))) {
   
   interation_time <- system.time({
     
@@ -122,3 +122,29 @@ for (i in seq_along(select_paths)) {
 }
 
 names(sas_data_list) <- file_path_sans_ext(basename(select_paths))
+
+eSituation_11_Diag_codes <- read_sas(select_paths[10], col_select = select_variables[[10]])
+
+# save.image(file = "all_variables.Rdata")
+
+# START HERE --------------------------------------------------------------
+
+library(tidyverse)
+library(haven)
+library(tools)
+library(purrr)
+library(lubridate)
+
+use_data <- reduce(sas_data_list, left_join, by = "PcrKey")
+
+select_data <- use_data %>% 
+  select(PcrKey:EMSTotalCallTimeMin, contains(c("Dispatch", "Disposition", "Situtiation", "Response", "Times", "Patient_", "Crew")))
+
+to_NA <- c("7701003", "7701001", "7701005", "Not Recorded", "Not Applicable", "")
+
+clean_NA <- select_data %>% 
+  mutate(across(everything(), ~ if_else(.x %in% to_NA, NA, .x))) %>% 
+  filter(
+    !is.na(ePatient_15), # and removing rows where age is NA
+    !is.na(ePatient_16)
+  )
