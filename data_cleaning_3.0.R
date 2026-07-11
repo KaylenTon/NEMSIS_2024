be6c20745052338eb1b24c0a462c5291eb88f248
Megan Xiao <xmegan10@gmail.com>
2026-07-11 11:39:17 -0400

    create cleaned duckdb tables

--- data_cleaning_3.0.R
+++ data_cleaning_3.0.R
@@ -8,22 +8,22 @@ library(tools)
 
 set.seed(67)
 
-time_path <- ("C:/Users/xmega/University of South Florida/Loni Hagen - data/SAS2024CP25/factpcrtime.sas7bdat")
-
-system.time(
-  keys <- read_sas(time_path, col_select = "PcrKey")
-  )
-
- system.time(
-   sample_keys_one_percent <- keys %>% 
-  slice_sample(prop = .01)
- )
+# time_path <- ("C:/Users/xmega/University of South Florida/Loni Hagen - data/SAS2024CP25/factpcrtime.sas7bdat")
+# 
+# system.time(
+#   keys <- read_sas(time_path, col_select = "PcrKey")
+#   )
+# 
+#  system.time(
+#    sample_keys_one_percent <- keys %>% 
+#   slice_sample(prop = .01)
+#  )
+#  
+#  #save sample_keys as a R data object
+#  save(sample_keys_one_percent, file = "sample_keys_one_percent.RData")
  
- #save sample_keys as a R data object
- save(sample_keys_one_percent, file = "sample_keys_one_percent.RData")
- 
 
- select_paths <- c("C:/Users/xmega/University of South Florida/Loni Hagen - data/SAS2024CP25/computedelements.sas7bdat",
+select_paths <- c("C:/Users/xmega/University of South Florida/Loni Hagen - data/SAS2024CP25/computedelements.sas7bdat",
                    "C:/Users/xmega/University of South Florida/Loni Hagen - data/SAS2024CP25/pub_pcrevents.sas7bdat",
                    "C:/Users/xmega/University of South Florida/Loni Hagen - data/SAS2024CP25/pcrpatientracegroup.sas7bdat",
                    "C:/Users/xmega/University of South Florida/Loni Hagen - data/SAS2024CP25/factpcrturnarounddelay.sas7bdat",
@@ -100,8 +100,10 @@ select_variables <- list(
        "DiagnosisCodeDescr")
    )
 
+#create a parquet folder to store parquet files
 dir.create("C:/Users/xmega/University of South Florida/Loni Hagen - data/parquet", showWarnings = FALSE)
 
+#for each SAS file, convert to SAS and store in the parquet folder
 for (p in select_paths){
   cat("Processing: ", basename(p), "\n")
   df <- read_sas(p)
@@ -135,6 +137,27 @@ for (f in files){
   datasets[[name]] <- open_dataset(f)
 }
 
+sample_keys <- datasets$factpcrtime %>%
+  select(PcrKey) %>%
+  distinct() %>%
+  collect() %>%
+  slice_sample(prop = 0.01)
+
+sample_keys_arrow <- arrow_table(sample_keys)
+
+sampled_datasets <- lapply(datasets, function(ds){
+  cols <- names(ds)
+  
+  #skip lookup tables without PcrKey
+  if (!"PcrKey" %in% cols){
+    return(ds)
+  }
+  
+  ds %>%
+    semi_join(sample_keys_arrow,
+              by="PcrKey")
+})
+
 ###############################
 #### CONNECTING TO DUCK DB ####
 ###############################
@@ -142,56 +165,62 @@ for (f in files){
 con <- dbConnect(duckdb(), dbdir = ":memory:")
 
 #register all datasets into duckdb
-for (name in names(datasets)){
-  duckdb_register_arrow(
-    con,
-    name,
-    datasets[[name]]
-  )
-}
-
-#sample a reproducible query of 1%
-sample_keys <- dbGetQuery(con, "
-                          SELECT PcrKey
-                          FROM factpcrtime
-                          ORDER BY RANDOM()
-                          LIMIT (
-                            SELECT CAST(COUNT(*) * 0.01 AS INTEGER)
-                            FROM factpcrtime
-                          )")
-
-#register sample to db
-dbWriteTable(con, "sample_keys", sample_keys, overwrite = TRUE)
-
-tables <- names(datasets)
-
-#remove eSituation_11 from tables; sampling it will not work - it is a description table, not a PCR table
-tables <- tables[-2]
-tables
-
-#create the sampled tables
-for (tbl in tables) {
+for(tbl in setdiff(names(sampled_datasets), "eSituation_11ref")) {
   
-  dbExecute(con, paste0("
-                        CREATE OR REPLACE TABLE ", tbl, "_sample AS
-                        SELECT t.*
-                        FROM ", tbl," t
-                        INNER JOIN sample_keys s
-                          ON t.PcrKey = s.PcrKey"))
+  try(
+    duckdb_register_arrow(
+      con,
+      paste0(tbl, "_sample"),
+      sampled_datasets[[tbl]]
+    ),
+    silent = TRUE
+  )
+  
 }
 
-#show current tables in db
-dbGetQuery(con, "SHOW TABLES")
+# #sample a reproducible query of 1%
+# sample_keys <- dbGetQuery(con, "
+#                           SELECT PcrKey
+#                           FROM factpcrtime
+#                           ORDER BY RANDOM()
+#                           LIMIT (
+#                             SELECT CAST(COUNT(*) * 0.01 AS INTEGER)
+#                             FROM factpcrtime
+#                           )")
+# 
+# #register sample to db
+# dbWriteTable(con, "sample_keys", sample_keys, overwrite = TRUE)
+# 
+# tables <- names(datasets)
+# 
+# #remove eSituation_11 from tables; sampling it will not work - it is a description table, not a PCR table
+# tables <- tables[-2]
+# tables
+# 
+# #create the sampled tables
+# for (tbl in tables) {
+#   
+#   dbExecute(con, paste0("
+#                         CREATE OR REPLACE TABLE ", tbl, "_sample AS
+#                         SELECT *
+#                         FROM ", tbl,"
+#                         SEMI JOIN sample_keys
+#                           USING(PcrKey)"))
+# }
+
+
+tables <- names(sampled_datasets)
+pcr_tables <- tables[-2]
 
 #create a record to show total rows vs unique keys of each 1% sampled table
-dup_summary <- lapply(tables, function(tbl) {
+dup_summary <- lapply(pcr_tables, function(tbl) {
   dbGetQuery(
     con,
     paste0(
-      "SELECT '", tbl, "' AS table_name,
+      "SELECT '", tbl, "_sample' AS table_name,
               COUNT(*) AS rows,
               COUNT(DISTINCT PcrKey) AS unique_keys
-       FROM ", tbl
+       FROM ", tbl, "_sample"
     )
   )
 }) |> dplyr::bind_rows()
@@ -202,14 +231,11 @@ print(dup_summary)
 #save names of tables with duplicates
 dup_tables <- dup_summary$table_name[dup_summary$rows > dup_summary$unique_keys]
 
-#redefine tables variable; get all current tables from the current DB connection
-tables <- dbListTables(con)
-
 #save to schemas object: for each table, get column name and information
-schemas <- lapply(tables, function(tbl) {
+schemas <- lapply(pcr_tables, function(tbl) {
   x <- dbGetQuery(
     con,
-    paste0("DESCRIBE ", tbl)
+    paste0("DESCRIBE ", tbl,"_sample")
   )
   
   x$table_name <- tbl
@@ -220,7 +246,10 @@ schemas <- do.call(rbind, schemas)
 
 schemas
 
+###################
 ### DEDUPLICATE ###
+###################
+
 #tables that must be deduplicated: factpcrdestinationteam, factpcrresponsedelay, factpcrscenedelay, factpcrturnarounddelay, pcrpatientracegroup
 dup_tables
 
@@ -280,14 +309,195 @@ variation_report <- subset(
   
 variation_report
 
+## RESULTS OF VARIATION REPORT ##
 
-# Fill in applicable null codes with NA
+#                                                    table_name            column_name keys_with_multiple_values
+# factpcrdestinationteam_sample.1 factpcrdestinationteam_sample        eDisposition_25                       201
+# factpcrdestinationteam_sample.2 factpcrdestinationteam_sample        eDisposition_24                       344
+# factpcrresponsedelay_sample       factpcrresponsedelay_sample           eResponse_09                      2640
+# factpcrscenedelay_sample             factpcrscenedelay_sample           eResponse_10                      3511
+# factpcrturnarounddelay_sample   factpcrturnarounddelay_sample           eResponse_12                      6950
+# pcrpatientracegroup_sample.1       pcrpatientracegroup_sample PcrPatientRaceGroupKey                      4549
+# pcrpatientracegroup_sample.2       pcrpatientracegroup_sample            ePatient_14                      4549
+
+### DUPLICATE COLUMNS - DO NOT RENAME YET ###
+# datetime_of_destination_prearrival_alert_or_activation = eDisposition_25
+
+# destination_team_prearrival_alert_or_activation = eDisposition_24
+# type_of_response_delay = eResponse_09
+# type_of_scene_delay = eResponse_10
+# type_of_turn_around_delay= eResponse_12
+# patient_race = ePatient_14
+####################################################
+
+#string columns merge with "_"
+str_cols <- c("eDisposition_24","eResponse_09","eResponse_10","eResponse_12","ePatient_14")
+
+#ePatient_14
+dbExecute(con, "CREATE OR REPLACE TABLE pcrpatientracegroup_clean AS
+          SELECT
+            PcrKey,
+            string_agg(DISTINCT CAST(ePatient_14 AS VARCHAR), '_') AS ePatient_14
+          FROM pcrpatientracegroup_sample
+          GROUP BY PcrKey;")
+
+#eResponse_09
+dbExecute(con, "CREATE OR REPLACE TABLE factpcrresponsedelay_clean AS
+          SELECT
+            PcrKey,
+            string_agg(DISTINCT CAST(eResponse_09 AS VARCHAR), '_') AS eResponse_09
+          FROM factpcrresponsedelay_sample
+          GROUP BY PcrKey;")
+
+#eResponse_10
+dbExecute(con, "CREATE OR REPLACE TABLE factpcrscenedelay_clean AS
+          SELECT
+            PcrKey,
+            string_agg(DISTINCT CAST(eResponse_10 AS VARCHAR), '_') AS eResponse_10
+          FROM factpcrscenedelay_sample
+          GROUP BY PcrKey;")
+
+#eResponse_12
+dbExecute(con, "CREATE OR REPLACE TABLE factpcrturnarounddelay_clean AS
+          SELECT
+            PcrKey,
+            string_agg(DISTINCT CAST(eResponse_12 AS VARCHAR), '_') AS eResponse_12
+          FROM factpcrturnarounddelay_sample
+          GROUP BY PcrKey;")
+
+#time column: keep the latest timestamp and create a new column calculating the difference between the latest and earliest timestamp
+time_cols <- c("eDisposition_25")
+
+#eDisposition_24 and eDisposition_25
+dbExecute(con, "CREATE OR REPLACE TABLE factpcrdestinationteam_clean AS
+SELECT
+    PcrKey,
+
+    string_agg(
+        DISTINCT CAST(eDisposition_24 AS VARCHAR),
+        '_'
+    ) AS eDisposition_24,
+
+    MAX(TRY_CAST(eDisposition_25 AS TIMESTAMP))
+        AS latest_eDisposition_25,
+
+    MIN(TRY_CAST(eDisposition_25 AS TIMESTAMP))
+        AS earliest_eDisposition_25,
+
+    datediff(
+        'minute',
+        MIN(TRY_CAST(eDisposition_25 AS TIMESTAMP)),
+        MAX(TRY_CAST(eDisposition_25 AS TIMESTAMP))
+    ) AS eDisposition_25_duration
+
+FROM factpcrdestinationteam_sample
+GROUP BY PcrKey;")
+
+### DEDUPLICATION END ###
+
+#########################################
+### INSPECTION OF DEDUPLICATED TABLES ###
+#########################################
+
+#Inspect cleaned deduplicated tables
+dbListTables(con)
+tables <- names(sampled_datasets)
+dedup_tables <- dbListTables(con)
+
+#inspect if the clean tables contain any more duplicated PcrKeys
+dedup_report <- list()
+
+for (tbl in dedup_tables){
+  
+  #get the columns from each table
+  cols <- dbGetQuery(
+    con,
+    paste0("DESCRIBE ", tbl)
+  )$column_name
+  
+  #remove PcrKey from each cols list
+  cols <- setdiff(cols, "PcrKey")
+  
+  column_results <- lapply(cols, function(col){
+    
+    query <- paste0("
+      SELECT
+        '", tbl, "' AS table_name,
+        '", col, "' AS column_name,
+        COUNT(*) AS keys_with_multiple_values
+      FROM (
+        SELECT PcrKey
+        FROM ", tbl, "
+        GROUP BY PcrKey
+        HAVING COUNT(DISTINCT ", col, ") > 1
+      )")
+    
+    dbGetQuery(con, query)
+    
+  })
+  
+  dedup_report[[tbl]] <- do.call(rbind, column_results)
+}
+
+
+variation_dedup_report <- do.call(rbind, dedup_report)
+
+variation_dedup_report <- subset(
+  variation_dedup_report,
+  keys_with_multiple_values > 0)
+
+variation_dedup_report
+
+#inspect rows containing "_"
+str_dedup_tables <- dedup_tables[-c("factpcrdestinationteam_clean")]
+
+for (tbl in str_dedup_tables){
+  
+  #get the columns from each table
+  cols <- dbGetQuery(
+    con,
+    paste0("DESCRIBE ", tbl)
+  )$column_name
+  
+  #remove PcrKey from each cols list
+  cols <- setdiff(cols, "PcrKey")
+  
+  column_results <- lapply(cols, function(col){
+    
+    query <- paste0("
+      SELECT * FROM ", tbl, "
+                    WHERE ", col, "
+                    LIKE '%_%")
+    
+    dbGetQuery(con, query)
+    
+  })
+}
+
+#nothing returned = deduplication succeeded
+
+#inspect factpcrdestinationteam separately
+dbGetQuery(con, "SELECT eDisposition_24 FROM factpcrdestinationteam_clean
+                  WHERE eDisposition_24
+                  LIKE '%_%'")
+dbGetQuery(con, "SELECT *
+FROM factpcrdestinationteam_clean
+WHERE eDisposition_25_duration IS NOT NULL
+LIMIT 100
+")
+
+############################
+### JOIN TABLES TOGETHER ###
+############################
+
+##################
+### REMOVE NAs ###
+##################    
 to_NA <- c("7701003", "7701001", "7701005", "Not Recorded", "Not Applicable", "")
 
-#rename columns
+################################
+### RENAME COLUMNS AND CODES ###
+################################
 
-#deduplicate tables with PcrKey duplicates
-
-#join tables together on PcrKey using an "_"
 
 #save as an R object
